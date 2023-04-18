fe4d2fbc053b96465ee40a7dbc3ba0ae5adafdc1
whc <772778995@qq.com>
2023-04-18 08:34:11 +0800

    移除 node 模块

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-import windFlexPlugin from './tailwind/plugin/flex'
-import windLineClampPlugin from './tailwind/plugin/lineClamp'
-export { windFlexPlugin, windLineClampPlugin }
-
-import windPreset from './tailwind/preset'
-import windElementProPreset from './tailwind/preset/elementProPreset'
-export { windPreset, windElementProPreset }
-
 import createEl from './el/createEl'
 export { createEl }
 
