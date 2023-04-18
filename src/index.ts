24620336ae4be05e79de14b569b37adcd96ba105
whc <772778995@qq.com>
2023-04-18 18:50:41 +0800

    0.0.11

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,11 @@ export { createEl }
 import uploadFile from './file/uploadFile'
 export { uploadFile }
 
+import Object from './types/Object'
+import ValueOf from './types/ValueOf'
 import DeepPartial from './types/DeepPartial'
-export { DeepPartial }
+import DeepRequired from './types/DeepRequired'
+import Enumerate from './types/Enumerate'
+import RangeNum from './types/RangeNum'
+
+export { Object, ValueOf, DeepPartial, DeepRequired, Enumerate, RangeNum }
