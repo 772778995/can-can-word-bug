899bd92306e0c870c8484add764863774de1c90f
whc <772778995@qq.com>
2023-04-18 17:35:44 +0800

    添加 DeepPartial 注释

--- src/types/DeepPartial.ts
+++ src/types/DeepPartial.ts
@@ -1,5 +1,19 @@
 /**
- * DeepPartial
+ * Partial 泛型深度化
+ * @example
+ * type Person = {
+ *  name: string
+ *  address: {
+ *    country: string
+ *  }
+ * }
+ * type PersonPartial = DeepPartial<Person>;
+ * // {
+ * //   name?: Deep<string | undefined>;
+ * //   address?: Deep<{
+ * //     country: string;
+ * //   } | undefined>;
+ * // }
  */
 type DeepPartial<T> = {
   [P in keyof T]?: T[P] extends (infer U)[]
