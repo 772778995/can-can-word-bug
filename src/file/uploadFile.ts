60da2b2df509f63e5755ad9398705590701dc3d6
whc <772778995@qq.com>
2023-04-20 13:54:25 +0800

    uploadFile 添加压缩功能

--- src/file/uploadFile.ts
+++ src/file/uploadFile.ts
@@ -1,10 +1,12 @@
 import createEl from '../el/createEl'
 import DeepPartial from '../types/DeepPartial'
 import merge from 'lodash/merge'
+import JsZip, { OutputType } from 'jszip'
+
+type TFile = File & { webkitRelativePath: string }
 
 /**
  * 上传文件
- * @param attrs input[type=file]的属性
  * @returns 选择的文件
  */
 const uploadFile = (() => {
@@ -18,24 +20,46 @@ const uploadFile = (() => {
   document.body.appendChild(fileInput)
   return async <
     Attrs extends DeepPartial<Omit<HTMLInputElement, 'type' | 'style'>> & { webkitdirectory?: boolean },
-    Res extends Attrs['multiple'] extends true
-    ? File[]
+    ZipOpts extends JsZip.JSZipGeneratorOptions<OutputType> | boolean,
+    Opts extends {
+      /** JsZip 参数，true 则为默认参数，false 则不压缩，默认为 false */
+      zipOpts?: ZipOpts
+    },
+    Res extends Opts['zipOpts'] extends true | ZipOpts ? string | Blob | number[] | Uint8Array | ArrayBuffer | Buffer :
+    Attrs['multiple'] extends true
+    ? TFile[]
     : Attrs['webkitdirectory'] extends true
-    ? File[]
-    : File
+    ? TFile[]
+    : TFile,
   >(
-    attrs = {} as Attrs
+    /** input[type=file]的属性 */
+    attrs = {} as Attrs,
+    /** 其他配置 */
+    opts = {} as Opts
   ): Promise<Res> => {
     merge(fileInput, attrs)
+    const { zipOpts } = merge({ zip: false }, opts)
+
     const res = (await new Promise((resolve, reject) => {
-      fileInput.onchange = e => {
+      fileInput.onchange = async e => {
+        const isMultiple = [attrs.multiple, attrs.webkitdirectory].includes(true)
         const target = e.target as HTMLInputElement
-        const files = target.files
-        if (files) {
-          if ([attrs.multiple, attrs.webkitdirectory].includes(true)) resolve([...((files as unknown) as File[])])
-          else resolve(files[0] as File)
-        } else reject(new Error('No file selected'))
+        const files = [target.files]
+        if (!files) return reject(new Error('No file selected'))
+        if (!isMultiple && files.length < 1) reject(new Error('No file selected'))
+        const fileList = [...files] as unknown as TFile[]
+
+        if (zipOpts) {
+          const jsZip = new JsZip()
+          for (const file of fileList) jsZip.file(file.webkitRelativePath, file)
+          const res = await jsZip.generateAsync(zipOpts === true ? {} : zipOpts)
+          return resolve(res)
+        }
+
+        if (isMultiple) return resolve(fileList)
+        if (!isMultiple) return resolve(fileList[0])
       }
+
       fileInput.onerror = reject
       fileInput.dispatchEvent(new MouseEvent('click'))
     }))
