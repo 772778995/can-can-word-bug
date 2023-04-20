0e707711b3e24751c67193e5f57233f563c23983
whc <772778995@qq.com>
2023-04-20 12:30:57 +0800

    0.0.12

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,10 @@ import uploadFile from './file/uploadFile'
 export { uploadFile }
 
 import Object from './types/Object'
+import Writable from './types/Writable'
 import ValueOf from './types/ValueOf'
 import DeepPartial from './types/DeepPartial'
 import DeepRequired from './types/DeepRequired'
 import Enumerate from './types/Enumerate'
 import RangeNum from './types/RangeNum'
-
-export { Object, ValueOf, DeepPartial, DeepRequired, Enumerate, RangeNum }
+export { Object, Writable, ValueOf, DeepPartial, DeepRequired, Enumerate, RangeNum }
