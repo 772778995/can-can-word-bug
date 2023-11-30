820646073b7ea17cb0c4afe75be4eed02bb2650e
whc <772778995@qq.com>
2023-11-30 18:52:29 +0800

    chore: 修改 dts 配置以及入口文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
 import { defineConfig } from 'vite'
 import packageJSON from './package.json'
+import ViteDTS from 'vite-plugin-dts'
 
 const pkgName = packageJSON.name.replace(/-\w/g, s => s[1].toUpperCase())
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [ViteDTS()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }]
   },
   build: {
     lib: {
-      name: pkgName,
-      fileName: pkgName,
+      name: packageJSON.name,
       entry: './src/index.ts',
-      formats: ['cjs', 'es', 'umd', 'iife']
+      formats: ['cjs', 'es', 'umd']
     }
   }
 })
