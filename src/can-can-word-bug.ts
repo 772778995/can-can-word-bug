21a4ab87ba1f321d738906fde9fe4fd2215e0017
whc <772778995@qq.com>
2023-12-19 20:01:23 +0800

    perf: 修改导出方式

--- src/can-can-word-bug.ts
+++ src/can-can-word-bug.ts
@@ -1,58 +1,40 @@
-import createEl from './el/createEl'
-import isElementVisible from './el/isElementVisible'
-export { createEl, isElementVisible }
+// element
+export { default as createEl } from './el/createEl'
+export { default as isElementVisible } from './el/isElementVisible'
 
-import getLocalForage from './storage/getLocalForage'
-export { getLocalForage }
+// storage
+export { default as getLocalForage } from './storage/getLocalForage'
 
-import getImgFileInfo from './img/getImgFileInfo'
-export { getImgFileInfo }
+// img
+export { default as getImgFileInfo } from './img/getImgFileInfo'
 
-import uploadFile from './file/uploadFile'
-export { uploadFile }
-import { getCsvStr, exportCsv } from './file/csv'
-export { getCsvStr, exportCsv }
+// file
+export { default as uploadFile } from './file/uploadFile'
+export { getCsvStr, exportCsv } from './file/csv'
 
-import getCrypto, { encrypt, decrypt } from './string/getCrypto'
-export { getCrypto, encrypt, decrypt }
-import getUUID from './string/getUUID'
-export { getUUID }
+// string
+export { default as getCrypto, encrypt, decrypt } from './string/getCrypto'
+export { default as getUUID } from './string/getUUID'
 
-import delay from './promise/delay'
-export { delay }
+// promise
+export { default as delay } from './promise/delay'
 
-import getType from './type/getType'
-export { getType }
+// type
+export { default as getType } from './type/getType'
 
-import Object from './types/Object'
-import Writable from './types/Writable'
-import WritableBy from './types/WritableBy'
-import ReadonlyBy from './types/ReadonlyBy'
-import ValueOf from './types/ValueOf'
-import PartialBy from './types/PartialBy'
-import DeepMerge from './types/DeepMerge'
-import DeepPartial from './types/DeepPartial'
-import RequiredBy from './types/RequiredBy'
-import DeepRequired from './types/DeepRequired'
-import Enumerate from './types/Enumerate'
-import RangeNum from './types/RangeNum'
-import EitherOr from './types/EitherOr'
-import ExtractCommonProps from './types/ExtractCommonProps'
-import HTMLElementTagName from './types/HTMLElementTagName'
-export type {
-  Object,
-  Writable,
-  WritableBy,
-  ReadonlyBy,
-  ValueOf,
-  PartialBy,
-  DeepMerge,
-  DeepPartial,
-  RequiredBy,
-  DeepRequired,
-  Enumerate,
-  RangeNum,
-  EitherOr,
-  ExtractCommonProps,
-  HTMLElementTagName
-}
+// dts
+export type { default as Object } from './types/Object'
+export type { default as Writable } from './types/Writable'
+export type { default as WritableBy } from './types/WritableBy'
+export type { default as ReadonlyBy } from './types/ReadonlyBy'
+export type { default as ValueOf } from './types/ValueOf'
+export type { default as PartialBy } from './types/PartialBy'
+export type { default as DeepMerge } from './types/DeepMerge'
+export type { default as DeepPartial } from './types/DeepPartial'
+export type { default as RequiredBy } from './types/RequiredBy'
+export type { default as DeepRequired } from './types/DeepRequired'
+export type { default as Enumerate } from './types/Enumerate'
+export type { default as RangeNum } from './types/RangeNum'
+export type { default as EitherOr } from './types/EitherOr'
+export type { default as ExtractCommonProps } from './types/ExtractCommonProps'
+export type { default as HTMLElementTagName } from './types/HTMLElementTagName'
